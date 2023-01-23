356dbee399045656bfdbeddc22952e50ec6fc89d
fabiotadpull <fabio@tadpull.com>
2023-01-23 15:14:48 -0300

    Added program code to run the challenge

--- CodecChallenge/Program.cs
+++ CodecChallenge/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodecChallenge;
+
+var X = 1;
+var Y = 1;
+var commands = "";
+
+var size = Console.ReadLine();
+commands = Console.ReadLine();
+
+if (!String.IsNullOrEmpty(size) )
+{
+    X = Convert.ToInt32(size.Split('x')[0]);
+    Y = Convert.ToInt32(size.Split('x')[1]);    
+}
+else
+{
+    Console.WriteLine("Please enter the Plateau Size (XxY)");
+    Console.ReadKey();
+    return;
+}
+
+if (!String.IsNullOrEmpty(commands))
+{
+
+    Plateau plateau = new Plateau(X, Y);
+    Robot robot = new Robot();
+    robot.Move(commands, plateau);
+    Console.WriteLine($"{robot.X},{robot.Y},{robot.Facing}");
+    Console.ReadKey();        
+}
+else
+{
+   Console.WriteLine("Error: Commands sequence is required!");
+   Console.ReadKey();
+}
+
+
+
+    
+
+
